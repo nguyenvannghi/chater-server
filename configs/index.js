e579a61496cb41963e8e9a73535d60198757927f
nghiweb <nghiweb@gmail.com>
2020-01-29 10:01:18 +0700

    chater server

--- configs/index.js
+++ configs/index.js
@@ -1,12 +1,12 @@
 module.exports = {
-    MONGO_URI: 'mongodb+srv://nghiweb:nghiweb1@34@kaiji-nljzm.mongodb.net/kaiju',
+    MONGO_URI: '',
     PORT: 5002,
-    SECRET_KEY: 'SECRET_KEY',
+    SECRET_KEY: '',
     EXPIRES_IN: '1d',
     UPDATE_DEST: 'images/',
     // + googe cloud storage
-    DEFAULT_BUCKET_NAME: 'staging.kaiju-9f6ba.appspot.com',
+    DEFAULT_BUCKET_NAME: '',
     GOOGLE_CLOUD_KEYFILE: '../configs/kaiju-19c18f09224c.json',
-    GOOGLE_CLOUD_PROJECT_ID: 'kaiju-9f6ba',
+    GOOGLE_CLOUD_PROJECT_ID: '',
     STOREAGE_GOOGLE_API: 'https://storage.googleapis.com/',
 };
